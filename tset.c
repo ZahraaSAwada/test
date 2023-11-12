5816a7aa4f0b96a1cd0c08d2b974d23848c3cfb1
ZahraaSAwada <zsa20@mail.aub.edu>
2023-11-12 18:02:25 +0200

    adding random

--- tset.c
+++ tset.c
@@ -9,5 +9,26 @@ int main(int argc, char const *argv[])
     }
 
 
+    
+            }
+        }if (!validSpell) { 
+            printf("%s loses! Spell not found in the list.\n", (currentPlayer == 1) ? player1Name : player2Name);
+            gameOver = true;
+        } else if (!isSpellRepeated(spell, usedSpells, numUsedSpells)) {
+            if (numUsedSpells > 0 && !doSpellsMatch(usedSpells[numUsedSpells - 1], spell)) {
+                printf("%s wins! Spells do not match.\n", (currentPlayer == 1) ? player2Name : player1Name);
+                gameOver = true;
+            } else {
+                usedSpells[numUsedSpells] = strdup(spell);
+                numUsedSpells++;
+
+                if (numUsedSpells == numSpells) {
+                    printf("%s wins! All spells used.\n", (currentPlayer == 1) ? player1Name : player2Name);
+                    gameOver = true;
+                }
+            }
+        } else {
+            printf("%s wins! 
+
     return 0;
 }
